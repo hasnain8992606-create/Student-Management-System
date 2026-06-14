2437830cdef8dc6e58a5be4632219a85661fb4de
= <=>
2026-06-14 08:57:23 -0700

    Updated file

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -6,21 +6,21 @@ function Index() {
     <>
       <nav className='flex justify-center px-[50px] py-[20px] sticky top-0'>
         <h2 className='text-[#38bdf8]'>MyPortfolio</h2>
-        <div>
+        {/* <div className='text-white'>
           <Link to="/home">Home</Link>        
           <Link to="/about">About</Link>        
           <Link to="/skills">Skills</Link>        
           <Link to="/projects">Projects</Link>        
           <Link to="/contact">Contact</Link>        
-          </div>
+          </div> */}
       </nav>
 
-      {/* Hero Section Start */}
+      {/* Hero Section Start
       <div className="hero">
         <h1>Hi, I'm <span>Developer</span></h1>
-      </div>
+      </div> */}
     </>
   )
 }
 
-export default Index
+export default Index;
